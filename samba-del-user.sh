b39f9a8e4ce83f76b2a2569a4f7be97bb074d26d
agu1922 <49005390+agusdc111@users.noreply.github.com>
2025-08-22 03:17:33 -0300

    Delete samba-del-user.sh

--- samba-del-user.sh
+++
@@ -1,5 +0,0 @@
-\
-    #!/usr/bin/env bash
-    set -Eeuo pipefail
-    # (script completo de eliminación de usuario; igual que el que ya compartiste)
-    # ... [código completo idéntico al tuyo] ...
